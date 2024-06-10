4823c02a7b9992e30d0bfb872e979ee59727bb2b
GengYunTien <129279600+GengYunTien@users.noreply.github.com>
2024-06-10 20:45:16 +0800

    Update and rename __init__.sh to pyhist.sh

---
+++ image_preprocessing/patch_generation/pyhist.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/sh
+#SBATCH -A MST109178      
+#SBATCH -J PyHIST
+#SBATCH -p ngs53G          
+#SBATCH -c 8         
+#SBATCH --mem=53g
+#SBATCH -o out.log           
+#SBATCH -e err.log          
+#SBATCH --mail-user=willytien88@gmail.com    
+#SBATCH --mail-type=BEGIN,END,FAIL             
+
+cd ./root
+source activate ./anaconda3/envs/PyHIST
+
+k=1
+
+for filename in ./root/TCGA-COAD_WSI/diagnosis_slides/mpp/*.svs
+do
+id=$(basename "$filename" .svs)
+echo "Processing slide $k: $id"
+output_path="./root/pyhist/TCGA-COAD/"
+python /home/u1307362/PyHIST/pyhist.py \
+--method "otsu" \
+--patch-size 512 \
+--content-threshold 0.85 \
+--output-downsample 2 \
+--mask-downsample 32 \
+--output "$output_path" \
+--save-patches \
+--save-mask \
+--save-tilecrossed-image \
+--info "verbose" \
+"$filename"
+k=$((k+1))
+done
